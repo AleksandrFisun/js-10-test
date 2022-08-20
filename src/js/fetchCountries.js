913d11308ead306fb06e4131ef6744d97412e72c
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-08-21 02:01:46 +0300

    123

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
 export default function search(country) {
   return fetch(
     `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,languages,flags,region`
-  ).then(response => {
-    return response.json();
-  });
+  ).then(response => response.json());
 }
